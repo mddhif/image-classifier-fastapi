187b6249c1b3d28512245a48e1e1aa991b12216c
Med <4957312+mddhif@users.noreply.github.com>
2025-03-14 16:54:23 +0700

    add logging to datadog ..

--- main.py
+++ main.py
@@ -6,16 +6,26 @@ from tensorflow.keras.preprocessing import image
 import numpy as np
 from io import BytesIO
 from PIL import Image
+import logging
 
 
+
+logging.basicConfig(
+    filename='/tmp/dd.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
-print("Loading the model ...")
+logger.info("Loading the model ...")
 model = MobileNetV2(weights="imagenet")
 
 def read_image(file) -> Image.Image:
-    print("Preprocessing and preparing the image")
+    logger.info("Preprocessing and preparing the image")
     img = Image.open(BytesIO(file)).convert("RGB")
     img = img.resize((224, 224))
     img_array = image.img_to_array(img)
@@ -25,13 +35,13 @@ def read_image(file) -> Image.Image:
 
 @app.post("/predict/")
 async def predict(file: UploadFile = File(...)):
-    print("Reading uploaded image ...")
+    logger.info("Reading uploaded image ...")
     img_array = read_image(await file.read())
     predictions = model.predict(img_array) 
     decoded_predictions = decode_predictions(predictions, top=3)[0]  
 
     results = [{"label": label, "probability": float(prob)} for (_, label, prob) in decoded_predictions]
-    print("Sending results back ...")
+    logger.info("Sending results back ...")
     return {"predictions": results}
 
 
